958248acfa6179322283a450ec8b513cd084f137
linh <linh@linhs-iMac.local>
2010-07-04 11:31:46 +0900

    Remove GUI

--- project/build/src/vscpp.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-
-class WickiProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.0.RC2"
-}
--- src/main/scala/vsccp/logic/Board.scala
+++ src/main/scala/vsccp/logic/Board.scala
@@ -2,8 +2,6 @@ package vsccp.logic
 
 import scala.collection.mutable.ListBuffer
 
-import Piece._
-
 /**
  * http://en.wikipedia.org/wiki/Xiangqi
  *
@@ -14,60 +12,60 @@ import Piece._
  * | Red
  * 0
  * Down
+
+ * '　' = \u3000: zenkaku Japanese space character is used for blank position
  */
 class Board(moves: List[(Int, Int)]) {
-  val pieces = new Array[Piece](9*10)
-  reset
+  private val pieces = new StringBuilder(
+    "車馬象士將士象馬車" +
+    "　　　　　　　　　" +  // <- \u3000
+    "　砲　　　　　砲　" +
+    "兵　兵　兵　兵　兵" +
+    "　　　　　　　　　" +
+    "　　　　　　　　　" +
+    "卒　卒　卒　卒　卒" +
+    "　炮　　　　　炮　" +
+    "　　　　　　　　　" +
+    "俥傌相仕帥仕相傌俥")
+
   for ((fromIndex, toIndex) <- moves) {
-    pieces(toIndex)   = pieces(fromIndex)
-    pieces(fromIndex) = NONE
+    pieces.setCharAt(toIndex, pieces.charAt(fromIndex))
+    pieces.setCharAt(fromIndex, '\u3000')
   }
 
   def this() = this(List())
 
-  def reset {
-    for (i <- 0 to 9*10 - 1) pieces(i) = NONE
+  //----------------------------------------------------------------------------
 
-    pieces(0) = RCHARIOT;
-    pieces(1) = RHORSE
-    pieces(2) = RELEPHANT
-    pieces(3) = RGUARD
-    pieces(4) = RGENERAL
-    pieces(5) = RGUARD
-    pieces(6) = RELEPHANT
-    pieces(7) = RHORSE
-    pieces(8) = RCHARIOT
-
-    pieces(19) = RCANNON
-    pieces(25) = RCANNON
-
-    pieces(27) = RSOLDIER
-    pieces(29) = RSOLDIER
-    pieces(31) = RSOLDIER
-    pieces(33) = RSOLDIER
-    pieces(35) = RSOLDIER
-
-    pieces(54) = BSOLDIER
-    pieces(56) = BSOLDIER
-    pieces(58) = BSOLDIER
-    pieces(60) = BSOLDIER
-    pieces(62) = BSOLDIER
-
-    pieces(64) = BCANNON
-    pieces(70) = BCANNON
-
-    pieces(81) = BCHARIOT
-    pieces(82) = BHORSE
-    pieces(83) = BELEPHANT
-    pieces(84) = BGUARD
-    pieces(85) = BGENERAL
-    pieces(86) = BGUARD
-    pieces(87) = BELEPHANT
-    pieces(88) = BHORSE
-    pieces(89) = BCHARIOT
+  override def toString = {
+    val ret = (0 until 90).foldLeft(new StringBuilder(" 0 1 2 3 4 5 6 7 8")) { (acc, i) =>
+      if (i % 9 == 0) {
+        acc.append("\n")
+        acc.append(i/9)
+      }
+      acc.append(colorize(pieces.charAt(i)))
+    }
+    ret.append("\n")
+    ret.toString
   }
 
-  //---------------------------------------------------------------------------
+  // http://www.developer.com/open/article.php/631241/Linux-Console-Colors--Other-Tricks.htm
+  def colorize(piece: Char) = {
+    if (isRed(piece)) ("\033[31;1m" + piece + "\033[0m") else piece.toString
+  }
+
+  // piece must not be '\u3000'
+  def isBlack(piece: Char) = "將士象車砲馬卒".contains(piece.toString)
+
+  def isRed(piece: Char) = "帥仕相俥炮傌卒".contains(piece.toString)
+
+  def isBlank(piece: Char) = (piece == '\u3000')
+
+  // piece1 and piece2 must not be '\u3000'
+  def isSameSide(piece1: Char, piece2: Char) =
+    (isBlack(piece1) && isBlack(piece2)) || (!isBlack(piece1) && !isBlack(piece2))
+
+  //----------------------------------------------------------------------------
 
   def move(m: (Int, Int)) = new Board(moves :+ m)
 
@@ -108,38 +106,30 @@ class Board(moves: List[(Int, Int)]) {
 
   def genMoves(black: Boolean): List[(Int, Int)] = {
     var ret = new ListBuffer[(Int, Int)]
-    for (i <- 0 to 9*10 - 1) {
+    for (i <- 0 until 9*10) {
       val p = pieces(i)
-      if (p != NONE) {
-        if (black) {
-          p match {
-            case BGENERAL  => genGeneralMoves(ret, p, i)
-            case BGUARD    => genGuardMoves(ret, p, i)
-            case BELEPHANT => genElephantMoves(ret, p, i)
-            case BHORSE    => genHorseMoves(ret, p, i)
-            case BCHARIOT  => genChariotMoves(ret, p, i)
-            case BCANNON   => genCannonMoves(ret, p, i)
-            case BSOLDIER  => genSoldierMoves(ret, p, i)
-            case _         => None
-          }
-        } else {
-          p match {
-            case RGENERAL  => genGeneralMoves(ret, p, i)
-            case RGUARD    => genGuardMoves(ret, p, i)
-            case RELEPHANT => genElephantMoves(ret, p, i)
-            case RHORSE    => genHorseMoves(ret, p, i)
-            case RCHARIOT  => genChariotMoves(ret, p, i)
-            case RCANNON   => genCannonMoves(ret, p, i)
-            case RSOLDIER  => genSoldierMoves(ret, p, i)
-            case _         => None
-          }
-        }
+      p match {
+        case '將' => genGeneralMoves(ret, p, i)
+        case '帥' => genGeneralMoves(ret, p, i)
+        case '士' => genGuardMoves(ret, p, i)
+        case '仕' => genGuardMoves(ret, p, i)
+        case '象' => genElephantMoves(ret, p, i)
+        case '相' => genElephantMoves(ret, p, i)
+        case '車' => genChariotMoves(ret, p, i)
+        case '俥' => genChariotMoves(ret, p, i)
+        case '砲' => genCannonMoves(ret, p, i)
+        case '炮' => genCannonMoves(ret, p, i)
+        case '馬' => genHorseMoves(ret, p, i)
+        case '傌' => genHorseMoves(ret, p, i)
+        case '兵' => genSoldierMoves(ret, p, i)
+        case '卒' => genSoldierMoves(ret, p, i)
+        case _    =>
       }
     }
     ret.toList
   }
 
-  private def genGeneralMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
+  private def genGeneralMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
     if (isBlack(piece)) {
       // Up
       if (index < 77) move(moves, piece, index, index + 9)
@@ -165,7 +155,7 @@ class Board(moves: List[(Int, Int)]) {
     }
   }
 
-  private def genGuardMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
+  private def genGuardMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
     if (isBlack(piece)) {
       // Up-right
       if (index == 66 || index == 66 + 9 + 1) move(moves, piece, index, index +  9 + 1)
@@ -187,67 +177,67 @@ class Board(moves: List[(Int, Int)]) {
     }
   }
 
-  private def genElephantMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
+  private def genElephantMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
     if (isBlack(piece)) {
       // Up-right
-      if ((index == 47 || index == 51 || index == 63 || index == 67) && pieces(index + 9 + 1) == NONE)
+      if ((index == 47 || index == 51 || index == 63 || index == 67) && pieces(index + 9 + 1) == '\u3000')
         move(moves, piece, index, index + 9*2 + 2)
       // Down-left
-      if ((index == 67 || index == 71 || index == 83 || index == 87) && pieces(index - 9 - 1) == NONE)
+      if ((index == 67 || index == 71 || index == 83 || index == 87) && pieces(index - 9 - 1) == '\u3000')
         move(moves, piece, index, index - 9*2 - 2)
       // Up-left
-      if ((index == 47 || index == 51 || index == 67 || index == 71) && pieces(index + 9 + 1) == NONE)
+      if ((index == 47 || index == 51 || index == 67 || index == 71) && pieces(index + 9 + 1) == '\u3000')
         move(moves, piece, index, index +  9*2 - 2)
       // Down-right
-      if ((index == 63 || index == 67 || index == 83 || index == 87) && pieces(index - 9 - 1) == NONE)
+      if ((index == 63 || index == 67 || index == 83 || index == 87) && pieces(index - 9 - 1) == '\u3000')
         move(moves, piece, index, index - 9*2 + 2)
     } else {
       // Up-right
-      if ((index ==  2 || index ==  6 || index == 18 || index == 22) && pieces(index + 9 + 1) == NONE)
+      if ((index ==  2 || index ==  6 || index == 18 || index == 22) && pieces(index + 9 + 1) == '\u3000')
         move(moves, piece, index, index + 9*2 + 2)
       // Down-left
-      if ((index == 22 || index == 26 || index == 38 || index == 42) && pieces(index - 9 - 1) == NONE)
+      if ((index == 22 || index == 26 || index == 38 || index == 42) && pieces(index - 9 - 1) == '\u3000')
         move(moves, piece, index, index - 9*2 - 2)
       // Up-left
-      if ((index ==  2 || index ==  6 || index == 22 || index == 26) && pieces(index + 9 + 1) == NONE)
+      if ((index ==  2 || index ==  6 || index == 22 || index == 26) && pieces(index + 9 + 1) == '\u3000')
         move(moves, piece, index, index + 9*2 - 2)
       // Down-right
-      if ((index == 18 || index == 22 || index == 38 || index == 42) && pieces(index - 9 + 1) == NONE)
+      if ((index == 18 || index == 22 || index == 38 || index == 42) && pieces(index - 9 + 1) == '\u3000')
         move(moves, piece, index, index - 9*2 + 2)
     }
   }
 
-  private def genHorseMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
+  private def genHorseMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
     val r = index/9
     val c = index%9
 
     // 1 o'clock
-    if (r < 8 && c < 8 && pieces(index + 9) == NONE) move(moves, piece, index, index + 9*2 + 1)
+    if (r < 8 && c < 8 && pieces(index + 9) == '\u3000') move(moves, piece, index, index + 9*2 + 1)
     // 2 o'clock
-    if (r < 9 && c < 7 && pieces(index + 1) == NONE) move(moves, piece, index, index + 9 + 2)
+    if (r < 9 && c < 7 && pieces(index + 1) == '\u3000') move(moves, piece, index, index + 9 + 2)
     // 4 o'clock
-    if (r > 0 && c < 7 && pieces(index + 1) == NONE) move(moves, piece, index, index - 9 + 2)
+    if (r > 0 && c < 7 && pieces(index + 1) == '\u3000') move(moves, piece, index, index - 9 + 2)
     // 5 o'clock
-    if (r > 1 && c < 8 && pieces(index - 9) == NONE) move(moves, piece, index, index - 9*2 + 1)
+    if (r > 1 && c < 8 && pieces(index - 9) == '\u3000') move(moves, piece, index, index - 9*2 + 1)
     // 7 o'clock
-    if (r > 0 && c > 0 && pieces(index - 9) == NONE) move(moves, piece, index, index - 9*2 - 1)
+    if (r > 0 && c > 0 && pieces(index - 9) == '\u3000') move(moves, piece, index, index - 9*2 - 1)
     // 8 o'clock
-    if (r > 1 && c > 1 && pieces(index - 1) == NONE) move(moves, piece, index, index - 9 - 2)
+    if (r > 1 && c > 1 && pieces(index - 1) == '\u3000') move(moves, piece, index, index - 9 - 2)
     // 10 o'clock
-    if (r < 9 && c > 1 && pieces(index - 1) == NONE) move(moves, piece, index, index + 9 - 2)
+    if (r < 9 && c > 1 && pieces(index - 1) == '\u3000') move(moves, piece, index, index + 9 - 2)
     // 11 o'clock
-    if (r < 8 && c > 0 && pieces(index + 9) == NONE) move(moves, piece, index, index + 9*2 - 1)
+    if (r < 8 && c > 0 && pieces(index + 9) == '\u3000') move(moves, piece, index, index + 9*2 - 1)
   }
 
-  private def genChariotMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
-    var p: Piece = null
+  private def genChariotMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
+    var p = '\u3000'
 
     // Up
     var i = index + 9
     var done = false
     while (!done && i < 9*10 - 1) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else if (!isSameSide(piece, p)) {
         move(moves, piece, index, i)
@@ -261,7 +251,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i >= 0) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else if (!isSameSide(piece, p)) {
         move(moves, piece, index, i)
@@ -276,7 +266,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i <= max) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else if (!isSameSide(piece, p)) {
         move(moves, piece, index, i)
@@ -291,7 +281,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i >= min) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else if (!isSameSide(piece, p)) {
         move(moves, piece, index, i)
@@ -301,15 +291,15 @@ class Board(moves: List[(Int, Int)]) {
     }
   }
 
-  private def genCannonMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
-    var p: Piece = null
+  private def genCannonMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
+    var p = '\u3000'
 
     // Up
     var i = index + 9
     var done = false
     while (!done && i < 9*10 - 1) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else done = true
       i += 9
@@ -317,7 +307,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i < 9*10 - 1) {
       p = pieces(i)
-      if (p != NONE) {
+      if (p != '\u3000') {
         if (!isSameSide(piece, p)) {
           move(moves, piece, index, i)
         }
@@ -331,7 +321,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i >= 0) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else done = true
       i -= 9
@@ -339,7 +329,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i >= 0) {
       p = pieces(i)
-      if (p != NONE) {
+      if (p != '\u3000') {
         if (!isSameSide(piece, p)) {
           move(moves, piece, index, i)
         }
@@ -354,7 +344,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i <= max) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else done = true
       i += 1
@@ -362,7 +352,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i <= max) {
       p = pieces(i)
-      if (p != NONE) {
+      if (p != '\u3000') {
         if (!isSameSide(piece, p)) {
           move(moves, piece, index, i)
         }
@@ -377,7 +367,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i >= min) {
       p = pieces(i)
-      if (p == NONE) {
+      if (p == '\u3000') {
         move(moves, piece, index, i)
       } else done = true
       i -= 1
@@ -385,7 +375,7 @@ class Board(moves: List[(Int, Int)]) {
     done = false
     while (!done && i >= min) {
       p = pieces(i)
-      if (p != NONE) {
+      if (p != '\u3000') {
         if (!isSameSide(piece, p)) {
           move(moves, piece, index, i)
         }
@@ -395,7 +385,7 @@ class Board(moves: List[(Int, Int)]) {
     }
   }
 
-  private def genSoldierMoves(moves: ListBuffer[(Int, Int)], piece: Piece, index: Int) {
+  private def genSoldierMoves(moves: ListBuffer[(Int, Int)], piece: Char, index: Int) {
     if (isBlack(piece)) {
       if (index > 44) {
         // Down
@@ -429,13 +419,13 @@ class Board(moves: List[(Int, Int)]) {
     var rR, rC, bR, bC = -1
     for (c <- 3 to 5) {
       for (r <- 0 to 2) {
-        if (pieces(r*9 + c) == RGENERAL) {
+        if (pieces(r*9 + c) == '帥') {
           rR = r
           rC = c
         }
       }
       for (r <- 7 to 9) {
-        if (pieces(r*9 + c) == BGENERAL) {
+        if (pieces(r*9 + c) == '將') {
           bR = r
           bC = c
         }
@@ -451,17 +441,17 @@ class Board(moves: List[(Int, Int)]) {
       false
     } else {
       for (i <- (rRow + 1) to (bRow - 1)) {
-        if (pieces(i*9 + rCol) != NONE) return false
+        if (pieces(i*9 + rCol) != '\u3000') return false
       }
       true
     }
   }
 
   // The position at toIndex will be checked if the piece can be landed.
-  private def move(moves: ListBuffer[(Int, Int)], piece: Piece, fromIndex: Int, toIndex: Int) {
+  private def move(moves: ListBuffer[(Int, Int)], piece: Char, fromIndex: Int, toIndex: Int) {
     if (toIndex >=0 && toIndex < 9*10) {
       val toPiece = pieces(toIndex)
-      val landable = (toPiece == NONE || !isSameSide(piece, toPiece))
+      val landable = (toPiece == '\u3000' || !isSameSide(piece, toPiece))
       if (landable) {
         val moves2 = moves.toList :+ (fromIndex, toIndex)
         val b = new Board(moves2)
@@ -476,43 +466,25 @@ class Board(moves: List[(Int, Int)]) {
   def toInt(black: Boolean) = {
     val forRed = pieces.foldLeft(0) { (s, e) =>
       s + (e match {
-        case NONE      => 0
-        case BGENERAL  => -9999
-        case BGUARD    => -20
-        case BELEPHANT => -20
-        case BHORSE    => -45
-        case BCHARIOT  => -90
-        case BCANNON   => -50
-        case BSOLDIER  => -10
-        case RGENERAL  => 9999
-        case RGUARD    => 20
-        case RELEPHANT => 20
-        case RHORSE    => 45
-        case RCHARIOT  => 90
-        case RCANNON   => 50
-        case RSOLDIER  => 10
+        case '\u3000'  => 0
+
+        case '將' => -9999
+        case '士' => -20
+        case '象' => -20
+        case '車' => -90
+        case '砲' => -50
+        case '馬' => -45
+        case '兵' => -10
+
+        case '帥' => 9999
+        case '仕' => 20
+        case '相' => 20
+        case '俥' => 90
+        case '炮' => 50
+        case '傌' => 45
+        case '卒' => 10
       })
     }
     if (black) -forRed else forRed
-  }
-
-  override def toString = {
-    var ret = "\n "
-    for (c <- 0 to 8) ret += c + "\t"
-    ret += "\n"
-    for (r <- 9 to 0 by -1) {
-      ret += r
-      for (c <- 0 to 8) {
-        val p = pieces(r*9 + c)
-        // http://www.developer.com/open/article.php/631241/Linux-Console-Colors--Other-Tricks.htm
-        val format = if (RGENERAL <= p && p <= RSOLDIER)
-          "\033[31;1m%s\033[0m"
-        else
-          "%s"
-        ret += format.format(p) + "\t"
-      }
-      ret += "\n\n\n\n"
-    }
-    ret
   }
 }
